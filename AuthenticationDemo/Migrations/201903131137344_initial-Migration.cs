5bd2ebda88ef5e1652b1dbb39bff6e09c5f216d5
payal-sharma-1606 <payal2534@gmail.com>
2019-03-13 18:34:45 +0530

    Commit initial
    Commit initial

---
+++ AuthenticationDemo/Migrations/201903131137344_initial-Migration.cs
@@ -0,0 +1,60 @@
+namespace AuthenticationDemo.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initialMigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        UserId = c.Int(nullable: false, identity: true),
+                        Username = c.String(),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        Email = c.String(),
+                        Password = c.String(),
+                        IsActive = c.Boolean(nullable: false),
+                        ActivationCode = c.Guid(nullable: false),
+                    })
+                .PrimaryKey(t => t.UserId);
+            
+            CreateTable(
+                "dbo.Roles",
+                c => new
+                    {
+                        RoleId = c.Int(nullable: false, identity: true),
+                        RoleName = c.String(),
+                    })
+                .PrimaryKey(t => t.RoleId);
+            
+            CreateTable(
+                "dbo.UserRoles",
+                c => new
+                    {
+                        UserID = c.Int(nullable: false),
+                        RoleID = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.UserID, t.RoleID })
+                .ForeignKey("dbo.Users", t => t.UserID, cascadeDelete: true)
+                .ForeignKey("dbo.Roles", t => t.RoleID, cascadeDelete: true)
+                .Index(t => t.UserID)
+                .Index(t => t.RoleID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropIndex("dbo.UserRoles", new[] { "RoleID" });
+            DropIndex("dbo.UserRoles", new[] { "UserID" });
+            DropForeignKey("dbo.UserRoles", "RoleID", "dbo.Roles");
+            DropForeignKey("dbo.UserRoles", "UserID", "dbo.Users");
+            DropTable("dbo.UserRoles");
+            DropTable("dbo.Roles");
+            DropTable("dbo.Users");
+        }
+    }
+}
